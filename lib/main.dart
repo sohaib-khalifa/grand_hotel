73536fedb87338714abf45500543bbe80a32df37
Jack234567890 <mohamedrocket984@gmail.com>
2026-03-04 02:46:03 +0200

    remove old import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:grand_hotel/features/onboarding/screens/Main_App_Screen.dart';
-import 'main_app_screen.dart';
 
 void main() {
   runApp(const GrandHotel());
